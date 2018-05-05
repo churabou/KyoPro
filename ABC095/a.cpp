d899432471a1ad5b4318e8172f11906f4e2aa8e5
churabou <ginga7888@gmail.com>
2018-05-05 18:20:13 +0900

    up

---
+++ ABC095/a.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+
+int main() {
+
+  string s;
+  cin >> s;
+
+  int price = 700;
+
+  for (int i=0; i<s.size(); i++) {
+    if (s[i] == 'o') price += 100;
+  }
+
+  cout << price << endl;
+  return 0;
+}
